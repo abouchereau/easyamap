375ee804221a5e9ef4dd062eeb84058badd2ae01
abouchereau <abouchereau@hotmail.com>
2022-08-23 17:45:20 +0200

    ajout tableau livraison multi amap

--- src/Repository/PurchaseRepository.php
+++ src/Repository/PurchaseRepository.php
@@ -7,11 +7,11 @@ use App\Entity\Purchase;
 
 class PurchaseRepository extends EntityRepository
 {
-  public function getProductsNextDistributionByUser($user, $date, $limit)
-  {
-    $distris = $this->getNextDistributions($date, $limit);
-    $conn = $this->getEntityManager()->getConnection();
-    $sql = "SELECT pd.fk_distribution AS id_distribution, 
+    public function getProductsNextDistributionByUser($user, $date, $limit)
+    {
+        $distris = $this->getNextDistributions($date, $limit);
+        $conn = $this->getEntityManager()->getConnection();
+        $sql = "SELECT pd.fk_distribution AS id_distribution, 
         pu.quantity, 
         CONCAT(ifnull(p.label,''), 
         ' ', 
@@ -25,24 +25,24 @@ class PurchaseRepository extends EntityRepository
       AND pd.fk_distribution IN (".implode(', ',  array_keys($distris)).")
       GROUP BY pd.fk_distribution, p.id_product
       ORDER BY d.date, f.sequence, p.sequence";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute(['id_user' => $user->getIdUser()]);
-    $products = $stmt->fetchAll(\PDO::FETCH_GROUP);
-    $tab = array();
-    foreach($distris as $id => $dateStr)
-    {
-      $tab[$dateStr] = array('date' => \DateTime::createFromFormat('Y-m-d', $dateStr));
-      if (isset($products[$id]))
-        $tab[$dateStr]['produits'] = $products[$id];
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['id_user' => $user->getIdUser()]);
+        $products = $stmt->fetchAll(\PDO::FETCH_GROUP);
+        $tab = array();
+        foreach($distris as $id => $dateStr)
+        {
+            $tab[$dateStr] = array('date' => \DateTime::createFromFormat('Y-m-d', $dateStr));
+            if (isset($products[$id]))
+                $tab[$dateStr]['produits'] = $products[$id];
+        }
+        return $tab;
     }
-    return $tab;
-  }
-  
-  public function getProductsNextDistributionByFarm($farm, $date, $limit)
-  {
-   $distris = $this->getNextDistributions($date, $limit);
-   $conn = $this->getEntityManager()->getConnection();
-   $sql = "SELECT pd.fk_distribution AS id_distribution, IFNULL(SUM(pu.quantity),0) AS quantity, CONCAT(ifnull(p.label,''), ' ', ifnull(p.unit,'')) AS produit
+
+    public function getProductsNextDistributionByFarm($farm, $date, $limit)
+    {
+        $distris = $this->getNextDistributions($date, $limit);
+        $conn = $this->getEntityManager()->getConnection();
+        $sql = "SELECT pd.fk_distribution AS id_distribution, IFNULL(SUM(pu.quantity),0) AS quantity, CONCAT(ifnull(p.label,''), ' ', ifnull(p.unit,'')) AS produit
       FROM product_distribution pd
       LEFT JOIN purchase pu ON pu.fk_product_distribution = pd.id_product_distribution
       LEFT JOIN product p   ON p.id_product = pd.fk_product
@@ -51,127 +51,127 @@ class PurchaseRepository extends EntityRepository
       AND pd.fk_distribution IN (".implode(', ',  array_keys($distris)).")
       GROUP BY pd.fk_distribution, p.id_product
       ORDER BY d.date, p.sequence";
-      $stmt = $conn->prepare($sql);
-      $stmt->execute(['id_farm' => $farm->getIdFarm()]);
-    $products = $stmt->fetchAll(\PDO::FETCH_GROUP);
-    $tab = array();
-    foreach($distris as $id => $dateStr)
-    {
-      $tab[$dateStr] = array('date' => \DateTime::createFromFormat('Y-m-d', $dateStr));
-      if (isset($products[$id]))
-        $tab[$dateStr]['produits'] = $products[$id];
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['id_farm' => $farm->getIdFarm()]);
+        $products = $stmt->fetchAll(\PDO::FETCH_GROUP);
+        $tab = array();
+        foreach($distris as $id => $dateStr)
+        {
+            $tab[$dateStr] = array('date' => \DateTime::createFromFormat('Y-m-d', $dateStr));
+            if (isset($products[$id]))
+                $tab[$dateStr]['produits'] = $products[$id];
+        }
+        // die(print_r($distris,1).' - '.print_r($tab,1));
+        return $tab;
     }
-   // die(print_r($distris,1).' - '.print_r($tab,1));
-    return $tab;
-  }
-    
-  
-  protected function getNextDistributions($date, $limit)
-  {
-    $conn = $this->getEntityManager()->getConnection();
-    $sql = "SELECT id_distribution, date 
+
+
+    protected function getNextDistributions($date, $limit)
+    {
+        $conn = $this->getEntityManager()->getConnection();
+        $sql = "SELECT id_distribution, date 
       FROM distribution 
       WHERE date >= :date
       LIMIT :limit";
-      $stmt = $conn->prepare($sql);
-      $stmt->execute(['date' => $date,'limit' => $limit]);
-    return $stmt->fetchAll(\PDO::FETCH_KEY_PAIR);
-  }
-  
-  public function getPurchase($ids_distributions, $id_user,$contract)
-  {
-    $conn = $this->getEntityManager()->getConnection();
-    $sql = "SELECT pd.id_product_distribution, p.quantity
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['date' => $date,'limit' => $limit]);
+        return $stmt->fetchAll(\PDO::FETCH_KEY_PAIR);
+    }
+
+    public function getPurchase($ids_distributions, $id_user,$contract)
+    {
+        $conn = $this->getEntityManager()->getConnection();
+        $sql = "SELECT pd.id_product_distribution, p.quantity
             FROM purchase p
             LEFT JOIN product_distribution pd ON p.fk_product_distribution = pd.id_product_distribution
             LEFT JOIN distribution d ON d.id_distribution = pd.fk_distribution
             WHERE d.id_distribution IN (".implode(',',$ids_distributions).")
             AND p.fk_user=:id_user
-            AND p.fk_contract=:id_contract";        
-      $stmt = $conn->prepare($sql);
-      $stmt->execute(['id_user' => $id_user,'id_contract' => $contract->getIdContract()]);
-     return $stmt->fetchAll(\PDO::FETCH_KEY_PAIR);
-  }
-  
-  public function emptyContract($id_contract, $id_user, $referent=null)
-  {
-   /* $list = array();
-    foreach ($product_distributions as $product_distribution)
+            AND p.fk_contract=:id_contract";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['id_user' => $id_user,'id_contract' => $contract->getIdContract()]);
+        return $stmt->fetchAll(\PDO::FETCH_KEY_PAIR);
+    }
+
+    public function emptyContract($id_contract, $id_user, $referent=null)
     {
-      $list[] = $product_distribution['id_product_distribution'];
-    }*/
-    //$list = array_column($product_distributions,'id_product_distribution');// PHP 5.5
-    $conn = $this->getEntityManager()->getConnection();
-    $params = [];
-    if ($referent == null || $referent->getIsAdmin()) {
-        $sql = "DELETE FROM purchase
+        /* $list = array();
+         foreach ($product_distributions as $product_distribution)
+         {
+           $list[] = $product_distribution['id_product_distribution'];
+         }*/
+        //$list = array_column($product_distributions,'id_product_distribution');// PHP 5.5
+        $conn = $this->getEntityManager()->getConnection();
+        $params = [];
+        if ($referent == null || $referent->getIsAdmin()) {
+            $sql = "DELETE FROM purchase
             WHERE fk_user=:id_user
             AND fk_contract=:id_contract";
             $params = ['id_user' => $id_user,'id_contract' => $id_contract];
-    } else {
-        $sql = "DELETE p FROM purchase p
+        } else {
+            $sql = "DELETE p FROM purchase p
             left join product_distribution pd on pd.id_product_distribution = p.fk_product_distribution
             left join product pr on pr.id_product = pd.fk_product
             left join farm f on f.id_farm = pr.fk_farm
             left join referent r on r.fk_farm = f.id_farm
             WHERE p.fk_user=:id_user
             and r.fk_user=:id_referent
-            AND p.fk_contract=:id_contract";        
+            AND p.fk_contract=:id_contract";
             $params = ['id_user' => $id_user,'id_contract' => $id_contract,"id_referent"=>$referent->getIdUser()];
+        }
+        $stmt = $conn->prepare($sql);
+        $nb = $stmt->execute($params);
+        return $nb;
     }
-      $stmt = $conn->prepare($sql);
-      $nb = $stmt->execute($params);
-    return $nb;
-  }
-  
-  public function add($user, $tab, $contract)
-  {
-    $em = $this->getEntityManager();
-    $ids_purchase = array();
-    foreach ($tab as $id_product_distribution => $quantity)
+
+    public function add($user, $tab, $contract)
     {
-      try 
-      {
-        $productDistribution = $em->getRepository('App\Entity\ProductDistribution')->find($id_product_distribution);
-      }
-      catch(Exception $e)
-      {
-        return false;
-      }
-      $purchase = new Purchase();
-      $purchase->setFkUser($user)
-        ->setFkProductDistribution($productDistribution)
-        ->setFkContract($contract)
-        ->setQuantity($quantity);
-      try 
-      {
-       $em->persist($purchase);
-       $em->flush();//à enlever pour la transaction ?       
-      }
-      catch(Exception $e)
-      {
-        return false;
-      }
-      $farm_id = $purchase->getFkProductDistribution()->getFkProduct()->getFkFarm()->getIdFarm();
-      if (!isset($ids_purchase[$farm_id])) {
-          $ids_purchase[$farm_id] = array();
-      }
-      $ids_purchase[$farm_id][] = $purchase->getIdPurchase();
+        $em = $this->getEntityManager();
+        $ids_purchase = array();
+        foreach ($tab as $id_product_distribution => $quantity)
+        {
+            try
+            {
+                $productDistribution = $em->getRepository('App\Entity\ProductDistribution')->find($id_product_distribution);
+            }
+            catch(Exception $e)
+            {
+                return false;
+            }
+            $purchase = new Purchase();
+            $purchase->setFkUser($user)
+                ->setFkProductDistribution($productDistribution)
+                ->setFkContract($contract)
+                ->setQuantity($quantity);
+            try
+            {
+                $em->persist($purchase);
+                $em->flush();//à enlever pour la transaction ?
+            }
+            catch(Exception $e)
+            {
+                return false;
+            }
+            $farm_id = $purchase->getFkProductDistribution()->getFkProduct()->getFkFarm()->getIdFarm();
+            if (!isset($ids_purchase[$farm_id])) {
+                $ids_purchase[$farm_id] = array();
+            }
+            $ids_purchase[$farm_id][] = $purchase->getIdPurchase();
+        }
+        return $ids_purchase;
     }
-    return $ids_purchase;
-  }
-  
-  public function getProductsToShip($dates, $farms=null)
-  {
-      if ($farms != null) {
-          $farms_id = array();
-          foreach ($farms as $farm) {
-              $farms_id[] = $farm->getIdFarm();
-          }
-      }
-      
-      $conn = $this->getEntityManager()->getConnection();
-      $sql = "SELECT * FROM (
+
+    public function getProductsToShip($dates, $farms=null)
+    {
+        if ($farms != null) {
+            $farms_id = array();
+            foreach ($farms as $farm) {
+                $farms_id[] = $farm->getIdFarm();
+            }
+        }
+
+        $conn = $this->getEntityManager()->getConnection();
+        $sql = "SELECT * FROM (
 SELECT 
 f.label AS entity,
 d.date AS date,
@@ -189,10 +189,10 @@ left join product pr on pr.id_product = pd.fk_product
 left join farm f on f.id_farm = pr.fk_farm
 left join distribution d2 ON d2.id_distribution = pd.fk_distribution_shift 
 WHERE d.date IN ('".implode("','",$dates)."')";
-      if($farms != null) {
-        $sql .= "AND pr.fk_farm IN (".implode(',',$farms_id).") ";
-      }
-$sql .= " group by f.label, d.date, pr.fk_farm, pr.id_product, f.sequence, pr.sequence, d2.date
+        if($farms != null) {
+            $sql .= "AND pr.fk_farm IN (".implode(',',$farms_id).") ";
+        }
+        $sql .= " group by f.label, d.date, pr.fk_farm, pr.id_product, f.sequence, pr.sequence, d2.date
 UNION
 SELECT 
 f.label AS entity,
@@ -212,10 +212,10 @@ left join farm f on f.id_farm = pr.fk_farm
 left join distribution d2 ON d2.id_distribution = pd.fk_distribution_shift 
 WHERE pu.fk_product_distribution IS NOT NULL
 AND d2.date IN ('".implode("','",$dates)."')";
-      if($farms != null) {
-        $sql .= "AND pr.fk_farm IN (".implode(',',$farms_id).") ";
-      }
-$sql .= "
+        if($farms != null) {
+            $sql .= "AND pr.fk_farm IN (".implode(',',$farms_id).") ";
+        }
+        $sql .= "
 group by f.label, d.date, pr.fk_farm, pr.id_product, f.sequence, pr.sequence, d2.date
 ) v
 ORDER BY v.f_seq, v.DATE, v.pr_seq, v.is_shift";
@@ -223,7 +223,7 @@ ORDER BY v.f_seq, v.DATE, v.pr_seq, v.is_shift";
         $r = $conn->query($sql);
         $tab = $r->fetchAll(\PDO::FETCH_GROUP);
         return $this->fetchGroupTwoLevels($tab);
-  }
+    }
 
     public function getProductsToShipMulti($dateDebut, $dateFin, $farms)
     {
@@ -234,7 +234,7 @@ ORDER BY v.f_seq, v.DATE, v.pr_seq, v.is_shift";
             $farm = $farms[$i];
             $sql .= "
 SELECT
-'".$farm['db']." - ' || f.label AS entity,
+concat((SELECT name FROM ".$farm['db'].".setting), ' - ', f.label) AS entity,
 d.date AS date,
 pr.fk_farm AS fk_farm, 
 sum(pu.quantity) AS nb, 
@@ -254,7 +254,7 @@ AND pr.fk_farm=".$farm['id_farm']."
  group by f.label, d.date, pr.fk_farm, pr.id_product, f.sequence, pr.sequence, d2.date
 UNION
 SELECT 
-'".$farm['db']." - ' || f.label AS entity,
+concat('".$farm['db']." - ', f.label) AS entity,
 d2.date AS date,
 pr.fk_farm AS fk_farm, 
 sum(pu.quantity) AS nb, 
@@ -288,7 +288,7 @@ ORDER BY v.f_seq, v.DATE, v.pr_seq, v.is_shift";
     }
 
     public function getProductsToRecover($dates, $id_user=null)
-  {
+    {
         $conn = $this->getEntityManager()->getConnection();
         $sql = "SELECT * FROM (
 SELECT 
@@ -340,26 +340,26 @@ SELECT
         $stmt->execute(['id_user'=>$id_user]);
         $tab = $stmt->fetchAll(\PDO::FETCH_GROUP);
         return $this->fetchGroupTwoLevels($tab);
-  }
-  
-  protected function fetchGroupTwoLevels($tab)
-  {
-      foreach ($tab as $entity => $infos)
-      {
-          $newInfo = array();
-          foreach($infos as $info)
-          {
-              if (!isset($newInfo[$info['date']]))
-                  $newInfo[$info['date']] = [];
-              $newInfo[$info['date']][] = ['nb'=>$info['nb'],'produit' => $info['produit'],'date_shift' => $info['date_shift'],'is_shift' => $info['is_shift']];
-          }
-          $tab[$entity] = $newInfo;
-      }
-      return $tab;
-  }
-  
-  public function getFarmsFromPurchases($ids_purchase) {//à tester
-       $conn = $this->getEntityManager()->getConnection();
+    }
+
+    protected function fetchGroupTwoLevels($tab)
+    {
+        foreach ($tab as $entity => $infos)
+        {
+            $newInfo = array();
+            foreach($infos as $info)
+            {
+                if (!isset($newInfo[$info['date']]))
+                    $newInfo[$info['date']] = [];
+                $newInfo[$info['date']][] = ['nb'=>$info['nb'],'produit' => $info['produit'],'date_shift' => $info['date_shift'],'is_shift' => $info['is_shift']];
+            }
+            $tab[$entity] = $newInfo;
+        }
+        return $tab;
+    }
+
+    public function getFarmsFromPurchases($ids_purchase) {//à tester
+        $conn = $this->getEntityManager()->getConnection();
         $sql = "SELECT 
             distinct(f.id_farm)
             FROM purchase p
@@ -372,12 +372,12 @@ SELECT
             and pa.received=0";//TODO requete preparee
         $r = $conn->query($sql);
         return$r->fetchAll(\PDO::FETCH_COLUMN);
-  }
-  
-  public function getAmountByDistribution($ids_purchase) {
-      $conn = $this->getEntityManager()->getConnection();
-      
-      $sql = "SELECT
+    }
+
+    public function getAmountByDistribution($ids_purchase) {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = "SELECT
           t.id_farm,
           t.date,
           ROUND(SUM(t.prix_total),2) AS amount,
@@ -397,14 +397,14 @@ SELECT
             WHERE p.id_purchase IN (".implode(',',$ids_purchase).")
 ) AS t
           GROUP BY t.id_farm, t.date, t.has_ratio";//TODO requete preparee
-      
-      $r = $conn->query($sql);
-      return $r->fetchAll(\PDO::FETCH_ASSOC);
-  }
-  
-  public function userPurchaseMonth() {
-      $conn = $this->getEntityManager()->getConnection();
-      $sql = 'SELECT
+
+        $r = $conn->query($sql);
+        return $r->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function userPurchaseMonth() {
+        $conn = $this->getEntityManager()->getConnection();
+        $sql = 'SELECT
                 u.lastname,
                 date_format(d.date, "%Y%m")
                 from user u
@@ -414,17 +414,17 @@ SELECT
                 where u.is_active=1
                 group by u.lastname, date_format(d.date, "%Y%m")
                 order by u.lastname';
-      $r = $conn->query($sql);
-      return $r->fetchAll(\PDO::FETCH_GROUP|\PDO::FETCH_COLUMN);
-  }
-  
-  public function getPurchaseCountSince($date, $products, $user) {
+        $r = $conn->query($sql);
+        return $r->fetchAll(\PDO::FETCH_GROUP|\PDO::FETCH_COLUMN);
+    }
+
+    public function getPurchaseCountSince($date, $products, $user) {
         $id_products = [];
         foreach($products as $product) {
             $id_products[] = $product->getIdProduct();
         }
-      $conn = $this->getEntityManager()->getConnection();
-      $sql = "select pr.label, pr.unit, sum(pu.quantity) as quantity
+        $conn = $this->getEntityManager()->getConnection();
+        $sql = "select pr.label, pr.unit, sum(pu.quantity) as quantity
                 from purchase pu
                 left join product_distribution pd on pd.id_product_distribution = pu.fk_product_distribution
                 left join distribution dis on dis.id_distribution = pd.fk_distribution
@@ -435,12 +435,12 @@ SELECT
                 group by pd.fk_product";//TODO requete preparee
         $stmt = $conn->prepare($sql);
         $stmt->execute(array('id_user'=>$user->getIdUser(), 'date'=>$date->format('Y-m-d')));
-       return $stmt->fetchAll(\PDO::FETCH_ASSOC); 
-  }
-  
-  public function getQuantities($id_farm,$date_debut,$date_fin,$id_user) {
-      $conn = $this->getEntityManager()->getConnection();
-      $sql = "select fullname, id_product, sum(quantity) as quantity, sum(price) as price from (
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function getQuantities($id_farm,$date_debut,$date_fin,$id_user) {
+        $conn = $this->getEntityManager()->getConnection();
+        $sql = "select fullname, id_product, sum(quantity) as quantity, sum(price) as price from (
             select concat(u.lastname,' ',u.firstname,'|',u.id_user) as fullname, 
             pr.id_product, 
             pr.sequence, 
@@ -479,7 +479,7 @@ SELECT
             'date_fin'=>$date_fin->format('Y-m-d'),
             'id_farm'=>$id_farm,
             'id_user'=>$id_user));
-        $tab = $stmt->fetchAll(\PDO::FETCH_ASSOC); 
+        $tab = $stmt->fetchAll(\PDO::FETCH_ASSOC);
         $out = array('by_user'=>[],'total_quantity'=>[],'total_price'=>[],'total_total_quantity'=>0,'total_total_price'=>0, 'product_list' => []);
         foreach ($tab as $line) {
             if (!isset($out['by_user'][$line['fullname']])) {
@@ -500,6 +500,6 @@ SELECT
 
         }
         return $out;
-  }
+    }
 
 }
