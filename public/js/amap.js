6346014b782fbb5344bc302c6e15983253cc3b62
abouchereau <anthonybouchereau@hotmail.com>
2026-06-29 17:24:41 +0200

    sw

--- public/js/amap.js
+++ public/js/amap.js
@@ -43,7 +43,7 @@ const validatePayment = (idPayment, checked,) => {
 
 (async () => {
     if ('serviceWorker' in navigator && window.location.href.indexOf("localhost") == -1) {
-        navigator.serviceWorker.register('service-worker-v1.1.js');
+        navigator.serviceWorker.register('service-worker-v1.2.js');
     }
 })();
 
